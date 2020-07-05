a132fc0eb6841045fb223f8f130c035aa48fcb26
unknown <uniqueguanhaohui@hotmail.com>
2020-07-06 00:31:30 +0200

    my assignment

---
+++ JsonStructure.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrapper
+{
+    class JsonStructure
+    {
+        public class movie
+        {
+            public int id { get; set; }
+            public string? url { get; set; }
+            public string? name { get; set; }
+            public string? type { get; set; }
+            public string? language { get; set; }
+            public string[]? genres { get; set; }
+            public string? status { get; set; }
+            public int? runtime { get; set; }
+            public DateTime? premiered { get; set; }
+            public string officialSite { get; set; }
+            public STschedule? schedule { get; set; }
+            public STrating? rating { get; set; }
+            public int? weight { get; set; }
+            public STnetwork? network { get; set; }
+            public STnetwork? webChannel { get; set; }
+            public STexternal? externals { get; set; }
+            public STimage? image { get; set; }
+            public string? summary { get; set; }
+            public int? updated { get; set; }
+            public STlink? _links { get; set; }
+        }
+        public class STschedule
+        {
+            public DateTime? time { get; set; }
+            public string[]? days { get; set; }
+        }
+        public class STrating
+        {
+            public Single? Average { get; set; }
+        }
+
+        public class STcountry
+        {
+            public string? Name { get; set; }
+            public string? Code { get; set; }
+            public string? Timezone { get; set; }
+        }
+
+        public class STnetwork
+        {
+            public int? Id { get; set; }
+            public string? Name { get; set; }
+            public STcountry? Country { get; set; }
+        }
+        public class STexternal
+        {
+            public int? Tvrage { get; set; }
+            public int? Thetvdb { get; set; }
+            public string? Imdb { get; set; }
+        }
+        public class STimage
+        {
+            public string? Medium { get; set; }
+            public string? Original { get; set; }
+        }
+        public class STlink
+        {
+            public STself? Self { get; set; }
+            public STpreviousepisode? Previousepisode { get; set; }
+        }
+        public class STself
+        {
+            public string? href { get; set; }
+        }
+        public class STpreviousepisode
+        {
+            public string? href { get; set; }
+        }
+        public class STcast
+        {
+            public STperson? Person { get; set; }
+            public STcharacter? Character { get; set; }
+            public bool? Self { get; set; }
+            public bool? Voice { get; set; }
+        }
+        public class STperson
+        {
+            public int? Id { get; set; }
+            public string? Url { get; set; }
+            public string? Name { get; set; }
+            public STcountry? Country { get; set; }
+            public DateTime? Birthday { get; set; }
+            public DateTime? Deathday { get; set; }
+            public string? Gender { get; set; }
+            public STimage? Image { get; set; }
+            public STlink? _links { get; set; }
+        }
+        public class STcharacter
+        {
+            public int? Id { get; set; }
+            public string? Url { get; set; }
+            public string? Name { get; set; }
+            public STimage? image { get; set; }
+            public STlink? _links { get; set; }
+
+        }
+        public class ResponseShow
+        {
+            public int? Id { get; set; }
+            public string? Name { get; set; }
+            public List<Actor>? Cast { get; set; }
+
+        }
+        public class Actor
+        {
+            public int? Id { get; set; }
+            public string? Name { get; set; }
+            public string? Birthday { get; set; }
+        }
+        public class Pagenation
+        {
+            public int? PageId { get; set; }
+            public List<ResponseShow> LsShow { get; set; }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace Scrapper
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            // set storage file location
+            string StoreFileName = "ShowCast_data.json";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            + "/" + StoreFileName;
+
+            // this boolean is used to control whether all show pages to be collected
+            bool b_collectAllPages = true;
+            int startPage = 195;
+            int endpage;
+            int page2get = 10;
+
+            if (b_collectAllPages)
+            {
+                // if yes, then the RESTclient will start from 0 and end until an 404 response is received
+                startPage = 0;
+                endpage = 1;
+            }
+            else
+            {
+                // other option is to specify a show page range
+                endpage = startPage + page2get;
+            }
+
+
+            //Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Begin timing.
+            stopwatch.Start();
+
+            // create an instance of Pagenation list which contains page id ,a list of shows and their cast info
+            List<JsonStructure.Pagenation> LsPage = new List<JsonStructure.Pagenation>();
+
+
+            for (int pageIndex = startPage; pageIndex < endpage; pageIndex++)
+            {
+                // create an Restclient instance for getting response from a webapi
+                RESTClient rClient = new RESTClient();
+
+                //create an instance single pagenation
+                JsonStructure.Pagenation thisPage = new JsonStructure.Pagenation();
+
+                // create an instance a list of show/cast according to the assignment requirement
+                List<JsonStructure.ResponseShow> lsShow = new List<JsonStructure.ResponseShow>();
+
+                // compose a url with variable pageindex
+                rClient.endPoint = "http://api.tvmaze.com/shows?page=" + pageIndex.ToString();
+
+                //1st step -  get response as json format from the api with this url
+                string strJSON = string.Empty;
+                strJSON = rClient.makeRequest();
+                if (strJSON.Contains("error: (404)"))
+                {
+                    // exit the show page for loop once receiving not found error HTTP404
+                    // this means the previous page was the last page that contains information
+                    endpage = pageIndex;
+                    break;
+                }
+                // deserialize json response into a list of shows
+                List<JsonStructure.movie> shows = (List<JsonStructure.movie>)DeserializeToList<JsonStructure.movie>(strJSON);
+
+                // 2nd step - get info of each show/movie on the show list
+                for (int i = 0; i < shows.Count; i++)
+                {
+                    // tunned rate limited of api reponse
+                    Thread.Sleep(200);
+                    // map show id and show name
+                    var showid = shows[i].id;
+                    var showName = shows[i].name;
+
+                    // construct url for each show with show id as variable
+                    rClient.endPoint = "http://api.tvmaze.com/shows/" + showid.ToString() + "/cast";
+                    var strJSON_cast = rClient.makeRequest();
+
+                    // deserialized json response into list a cast for this show
+                    List<JsonStructure.STcast> lscast = (List<JsonStructure.STcast>)DeserializeToList<JsonStructure.STcast>(strJSON_cast);
+
+                    // sort the cast of this show by birthday descending 
+                    var descListOb = lscast.OrderByDescending(x => x.Person.Birthday);
+                    var lsCastDec = descListOb.ToList();
+
+                    // create an instance of show/cast
+                    JsonStructure.ResponseShow ThisShow = new JsonStructure.ResponseShow();
+                    ThisShow.Id = showid;
+                    ThisShow.Name = showName;
+
+                    //create an instance of actor list and fill it with required actor info
+                    List<JsonStructure.Actor> lsActor = new List<JsonStructure.Actor>();
+                    for (int IndexActor = 0; IndexActor < lsCastDec.Count; IndexActor++)
+                    {
+                        var ActorName = lsCastDec[IndexActor].Person.Name;
+                        var ActorBirthday = String.Format("{0:yyyy-MM-dd}", lsCastDec[IndexActor].Person.Birthday);
+                        var ActorId = lsCastDec[IndexActor].Person.Id;
+
+                        JsonStructure.Actor actor = new JsonStructure.Actor();
+                        actor.Id = ActorId;
+                        actor.Name = ActorName;
+                        actor.Birthday = ActorBirthday;
+
+                        lsActor.Add(actor);
+
+                    }
+
+                    ThisShow.Cast = lsActor;
+
+                    // add this show with show name,id, list of cast(actor name, id, birthday)
+                    lsShow.Add(ThisShow);
+
+                    if (b_collectAllPages)
+                    {
+                        endpage += 1;
+                    }
+                }
+                // complet page info (page id and list of show)
+                thisPage.PageId = pageIndex;
+                thisPage.LsShow = lsShow;
+                Console.WriteLine("Page " + pageIndex.ToString() + " is done");
+
+                // add this page to page list
+                LsPage.Add(thisPage);
+
+            }
+
+            // last step - store all information by as json string in a text file
+            string Json2Str = JsonConvert.SerializeObject(LsPage, Formatting.Indented);
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(Json2Str);
+
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(Json2Str);
+                }
+            }
+
+            // Stop timing.
+            stopwatch.Stop();
+
+            // Write result.
+            Console.WriteLine("Time elapsed for " + (endpage - startPage).ToString() + "  pages: {0}", stopwatch.Elapsed);
+            Console.ReadLine();
+
+        }
+        private static void Jsonparser()
+        {
+
+
+
+        }
+        
+      
+        public static List<string> InvalidJsonElements;
+        public static IList<T> DeserializeToList<T>(string jsonString)
+        {
+            InvalidJsonElements = null;
+            var array = JArray.Parse(jsonString);
+            IList<T> objectsList = new List<T>();
+            foreach (var item in array)
+            {
+                try
+                {
+                    // CorrectElements
+                    objectsList.Add(item.ToObject<T>());
+                }
+                catch (Exception ex)
+                {
+                    InvalidJsonElements = InvalidJsonElements ?? new List<string>();
+                    InvalidJsonElements.Add(item.ToString());
+                }
+            }
+            return objectsList;
+        }
+    }
+}
